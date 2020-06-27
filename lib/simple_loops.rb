f7b622eef69aeab2386cec074b7fa80c03b50147
Cody C <codyicastro@gmail.com>
2020-06-27 20:36:33 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,15 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  message = "Hello World."
+  5.times {puts message}
+end
+
+def loop_message_n_times(message,num=5)
+  count = 0
+    puts "#{message}"
+    count += 1 
+    until num == count
+  end
+
+loop_message_n_times("Hello World.", 10)
